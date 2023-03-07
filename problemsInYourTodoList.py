048ae84ded1d5d1557bdd50fa63fd8fd61c1952c
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-03-07 17:09:47 +0530

    Delete problemsInYourTodoList.py

--- problemsInYourTodoList.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for i in range(T):
-    c = 0
-    N = int(input())
-    D = list(map(int,input().split()))
-    for i in D:
-        if i >= 1000:
-            c += 1
-    print(c)
